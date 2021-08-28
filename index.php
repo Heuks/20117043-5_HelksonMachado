cfd9c5a61bf2ea110721ed87934568f5d87e9be7
Heuks <89207993+Heuks@users.noreply.github.com>
2021-08-27 21:06:57 -0300

    CSS, Index e Imagens

---
+++ index.php
@@ -0,0 +1,72 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+  </head>
+  <body>
+    <div id="menu">
+      <ul>
+        <li><a href="index.php">Início</a></li>
+        <li><a href="pages/quemsomos.html">Quem somos</a></li>
+        <li><a href="pages/contato.html">Contato</a></li>
+        <li><a href="pages/localizacao.html">Localização</a></li>
+      </ul>
+    </div>
+
+    <section class="produtos">
+      <section class="produto">
+        <a href="pages/produtos/item1.html"><img class="icone" src="images/item1.jpg" alt="Teste">
+        <p>Item 1</p>
+        </a>
+      </section>
+
+      <section class="produto">
+        <a href="pages/produtos/item2.html"><img class="icone" src="images/item2.jpg" alt="Teste">
+        <p>Item 2</p>
+        </a>
+      </section>
+
+      <section class="produto">
+        <a href="pages/produtos/item3.html"><img class="icone" src="images/item3.jpg" alt="Teste">
+        <p>Item 3</p>
+        </a>
+      </section>
+
+      <section class="produto">
+        <a href="pages/produtos/item4.html"><img class="icone" src="images/item4.jpg" alt="Teste">
+        <p>Item 4</p>
+        </a>
+      </section>
+    </section>
+
+    <section class="produtos">
+      <section class="produto">
+        <a href="pages/produtos/item5.html"><img class="icone" src="images/item5.jpg" alt="Teste">
+        <p>Item 5</p>
+        </a>
+      </section>
+
+      <section class="produto">
+        <a href="pages/produtos/item6.html"><img class="icone" src="images/item6.jpg" alt="Teste">
+        <p>Item 6</p>
+        </a>
+      </section>
+
+      <section class="produto">
+        <a href="pages/produtos/item7.html"><img class="icone" src="images/item7.jpg" alt="Teste">
+        <p>Item 7</p>
+        </a>
+      </section>
+
+      <section class="produto">
+        <a href="pages/produtos/item8.html"><img class="icone" src="images/item8.jpg" alt="Teste">
+        <p>Item 8</p>
+        </a>
+      </section>
+    </section>
+    
+      <footer class="rodape">Helkson Gonzaga Machado<br>Backend I - Sistema para Internet<br>Unicesumar - 2021</footer>
+
+
+  </body>
+</html>
